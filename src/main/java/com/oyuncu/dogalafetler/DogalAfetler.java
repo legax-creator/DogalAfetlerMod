40e038305c60637aea6856d950b817cb2f4725db
legax-creator <my3reeder@gmail.com>
2026-07-20 16:11:15 +0300

    Update DogalAfetler.java

--- src/main/java/com/oyuncu/dogalafetler/DogalAfetler.java
+++ src/main/java/com/oyuncu/dogalafetler/DogalAfetler.java
@@ -1,27 +1,45 @@
 package com.oyuncu.dogalafetler;
 
+import com.oyuncu.dogalafetler.client.WindHudOverlay;
+import com.oyuncu.dogalafetler.weather.WeatherSystem;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-@Mod(DogalAfetler.MODID)
+@Mod("dogalafetler")
 public class DogalAfetler {
-    // build.gradle ve mods.toml ile birebir aynı olmalı
-    public static final String MODID = "dogalafetler"; 
-    public static final Logger LOGGER = LogManager.getLogger();
 
     public DogalAfetler() {
-        // Modun başlangıç kurulum etkinliklerini dinliyoruz
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // İleride yazacağımız afet yöneticisini Forge'un ana etkinlik otobüsüne kaydediyoruz
+        // Forge event bus yapısını kaydediyoruz
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-        LOGGER.info("Dinamik Dogal Afetler Modu Aktif Edildi!");
+    // 1. DÜNYA DÖNGÜSÜ: Rüzgar ve mevsimlerin her gün arka planda güncellenmesi
+    @Mod.EventBusSubscriber(modid = "dogalafetler", bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class ForgeEvents {
+        @SubscribeEvent
+        public static void onWorldTick(TickEvent.LevelTickEvent event) {
+            // Sadece sunucu tarafında ve tick sonlandığında rüzgarı günceller
+            if (event.phase == TickEvent.Phase.END && !event.level.isClientSide()) {
+                WeatherSystem.updateWeather(event.level);
+            }
+        }
+    }
+
+    // 2. HUD KAYDI: Rüzgar göstergesinin Hotbar sağına çizilmesi (Sadece Client/İstemci tarafında çalışır)
+    @Mod.EventBusSubscriber(modid = "dogalafetler", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void registerOverlays(RegisterGuiOverlaysEvent event) {
+            // Arayüzümüzü en üste katman olarak ekliyoruz
+            event.registerAboveAll("wind_hud", WindHudOverlay.HUD_WIND);
+        }
     }
 }
